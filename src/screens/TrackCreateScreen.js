0274c58e2f54b73d4ec598833deed1df965d15c1
torrent benoit <torrentdu64@gmail.com>
2019-12-05 09:22:06 +1300

    wired to app.js the deferent screen

---
+++ src/screens/TrackCreateScreen.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+import {View
+  ,StyleSheet,
+  Text
+} from 'react-native';
+
+const TrackCreateScreen = (props) => {
+  render() {
+    return (
+      <View >
+        Text>track create </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default TrackCreateScreen;
