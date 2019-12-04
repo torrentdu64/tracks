50556aadb03eac92140cee2f05c5bec8ea9ba523
torrent benoit <torrentdu64@gmail.com>
2019-12-05 09:37:12 +1300

    route sceen in the app.js

--- App.js
+++ App.js
@@ -1,6 +1,31 @@
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import AccountScreen from './src/screens/AccountScreen';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/AccountScreen';
 import TrackCreateScreen from './src/screens/TrackCreateScreen';
 import TrackDetailScreen from './src/screens/TrackDetailScreen';
-import TackListScreen from './src/screens/TackListScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
+
+const switchNavigator = createSwitchNavigator({
+  loginFlow: createStackNavigator({
+    Signin: SigninScreen,
+    Signup: SignupScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
+
+export default createAppContainer(switchNavigator);
+
+
